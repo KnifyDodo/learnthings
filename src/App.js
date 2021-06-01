a633026b486f993b885df9c4cae094a141770b0e
KnifyDodo <knifydodo@gmail.com>
2021-06-01 21:13:46 +0200

    fuck it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TodoList from './TodoList';
+
 
 function App() {
+  const [todos, setTodos] = useState(['Todo 1', 'Todo 2'])
   return (
-    <TodoList />
+    <>
+      <TodoList todos={todos} />
+      <input type="text" />
+      <button>Add Todo</button>
+      <button>Clear Todos</button>
+      <div>0 left</div>
+    </>
   )
 }
 
